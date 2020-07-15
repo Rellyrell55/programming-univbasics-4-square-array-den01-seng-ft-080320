52887710f9f31b6ad3e4ba6ced8083bd913d467a
Terrell Cooper <terrellcoop@gmail.com>
2020-07-15 23:38:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   i = 0 
   new_array = []
   while i < array.length do 
-   array[i] * array[i]
+   new_array.push(array[i] * array[i])
    i += 1 
-   new_array.push(square_array)
   end
   return new_array 
 end 
