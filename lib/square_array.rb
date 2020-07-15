bcbf6d972c0b228b9de105d51a4da5861c11408a
Terrell Cooper <terrellcoop@gmail.com>
2020-07-15 22:33:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
-  Enumerator.new do |y|
-    array.each { |e| y << e ** 2 }    
-  end
-  .take(array.length)
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
 
-my_arr = [1, 2, 3, 4]
-p square_array(my_arr)
+arr = [1, 2, 3, 4, 5]
+puts square_array(arr)
