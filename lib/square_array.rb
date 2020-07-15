77fd20d9f3e7f3c40a68127cc2410abbf57ef25c
Terrell Cooper <terrellcoop@gmail.com>
2020-07-15 23:26:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
+  i = 0 
   new_array = []
-  while new_array.length < array.length do 
+  while i < array.length do 
    array{|a| new_array.push(a*a)}
   return new_array
 end
