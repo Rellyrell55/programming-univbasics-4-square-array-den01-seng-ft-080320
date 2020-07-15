13a3f8346d9b7a5374cb3ff8843779de2624da1e
Terrell Cooper <terrellcoop@gmail.com>
2020-07-15 21:29:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
  i = 0 
+ new_array = []
   while array[i] < array.length do 
-    return Math.sqrt(array[i]).times(3)
+    return Math.sqrt(array[i])
+    new_array[array[i]]
     i += 1
   end 
 end
